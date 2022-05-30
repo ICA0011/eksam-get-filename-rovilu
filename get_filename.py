ccb612bdf2b24a1f6d7f137f690a557c128a41bf
romanvilu <rovilu@ttu.ee>
2022-05-30 13:41:09 +0300

    Added first ex

--- get_filename.py
+++ get_filename.py
@@ -1,10 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = "http://upload.itcollege.ee/~aleksei/random_files/"
+URL = "http://upload.itcollege.ee/~aleksei/random_files/"
 
-def get_filename(url):
-  
-  # your code here
-  
-  return filename
+
+def get_filename(URL):
+    elements = BeautifulSoup(requests.get(URL).text, "html.parser").find_all("a")
+    count = 0
+    for body in elements:
+        filename = body.text
+        if "file" in filename:
+            if count == 10:
+                return filename
+            count += 1
+
+
+if __name__ == "__main__":
+    print("File with index of 10 is:", get_filename(URL))
